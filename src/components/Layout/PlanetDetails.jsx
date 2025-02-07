5b0382908f52325f8f39de6046618495a1085e80
ZISHANARIF <zishanarif912@gmail.com>
2025-02-07 18:17:36 +0530

    update planet  json file

--- src/components/Layout/PlanetDetails.jsx
+++ src/components/Layout/PlanetDetails.jsx
@@ -7,21 +7,24 @@ export const PlanetDetails = () => {
   const { id } = useParams(); // Get planet ID from URL
   const planet = PlanetDet.find(p => p.id === parseInt(id)); // Find planet by ID
 
-  if (!planet) {
-    return <h2 className="error-message">Planet details not found.</h2>;
-  }
+  // if (!planet) {
+  //   return <h2 className="error-message">Planet details not found.</h2>;
+  // }
+  const formattedAns = planet.DetailsExplaination.join("\n");
 
   return (
     <section className='country-section'>
       <div className='container-card bg-white-box'>
         <div className='countryInfo containerPla'>
-        {/* <img src={imagee}  className='planet-image'/> */}
           <p className='card-title'>NAME: {planet.Name}</p>
           <p className='card-description'>DISCOVERY YEAR: {planet.DiscoveryYear}</p>
           <p className='card-description'>ORBIT PERIOD: {planet.OrbitPeriod}</p>
           <p className='card-description'>DIAMETER: {planet.Diameter}</p>
-          <p className='card-title'><span>Details: </span>{planet.DetailsExplaination}</p>
-          <p ><span>EXAMPLE: </span>{planet.Example}</p>
+          <p className='card-description'><span>EXAMPLE: </span>{planet.Example}</p>
+          <pre className='card-title'><span>Details: </span>{formattedAns}</pre>
+          
+          {/* <pre className= 'card-title'><span>Ans: </span>{formattedAns}</pre> */}
+
           <NavLink to="/about">
             <button><FaArrowLeft /> Go Back</button>
           </NavLink>
