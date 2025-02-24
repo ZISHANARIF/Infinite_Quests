7c7e7fa5e13d13a622c7503fbc24f9a2a9f1016d
ZISHANARIF <zishanarif912@gmail.com>
2025-02-24 13:49:24 +0530

    RESUME

--- src/components/UI/Footers.jsx
+++ src/components/UI/Footers.jsx
@@ -49,7 +49,7 @@ export const Footers = () => {
                 </li>
                 <li>
                   <NavLink
-                    to="/Images/ZISHAN_ARIF_RESUME.pdf"
+                    to="/Images/ZISHAN_ARIF(RESUME).pdf"
                     target="_blank"
                   >
                     Resume
